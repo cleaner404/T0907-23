fefcfaa4d4bd3595275ec1765e23d857e0798731
cleaner208 <463187299@qq.com>
2016-09-07 08:19:01 +0800

    set a listener to check whether the phone book has changed.

---
+++ app/src/main/java/com/example/cleaner208/t0906_23/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cleaner208.t0906_23;
+
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG="Tag";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        getContentResolver().registerContentObserver(ContactsContract.Contacts.CONTENT_URI,true,new ContactsObserver(new Handler()));
+    }
+
+    private final class ContactsObserver extends ContentObserver {
+        public ContactsObserver(Handler handler){
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            Log.v(TAG,"Contact data is changed.");
+        }
+    }
+}
